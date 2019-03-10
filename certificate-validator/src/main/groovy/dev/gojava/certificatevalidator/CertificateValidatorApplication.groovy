ac4e85edde29f7f3d5109c20edb6a726da07a280
Alex Rabelo Ferreira <arf92@live.com>
2019-03-10 11:04:21 -0300

    #1 Adição de projeto spring boot
    O projeto foi criado utilizando o site initilzr com as seguintes configs:
    
    * devtools, security, jpa e groovy

---
+++ certificate-validator/src/main/groovy/dev/gojava/certificatevalidator/CertificateValidatorApplication.groovy
@@ -0,0 +1,13 @@
+package dev.gojava.certificatevalidator
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CertificateValidatorApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CertificateValidatorApplication, args)
+	}
+
+}
---
+++ certificate-validator/src/test/groovy/dev/gojava/certificatevalidator/CertificateValidatorApplicationTests.groovy
@@ -0,0 +1,16 @@
+package dev.gojava.certificatevalidator
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class CertificateValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
