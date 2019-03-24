b9e49cc4ea6635f85e2d629f0953e0a25097698c
Alex Rabelo Ferreira <arf92@live.com>
2019-03-24 12:38:14 -0300

    #2 Adição de pacotes padrões
    * adicao de classe padrão de teste com spock

---
+++ certificate-validator/src/test/groovy/dev/gojava/certificatevalidator/ui/controller/HomeControllerTest.groovy
@@ -0,0 +1,17 @@
+package dev.gojava.certificatevalidator.ui.controller
+
+import groovy.transform.CompileStatic
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+@CompileStatic
+@EqualsAndHashCode(callSuper = true)
+@ToString(includeFields = true, includeSuper = true)
+class HomeControllerTest extends spock.lang.Specification {
+
+	void setup() {
+	}
+
+	def "Index"() {
+	}
+}
