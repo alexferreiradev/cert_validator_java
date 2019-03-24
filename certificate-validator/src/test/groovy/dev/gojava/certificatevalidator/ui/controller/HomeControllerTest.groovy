90791441d1557a290a1cd1308257a4614e0f6213
Alex Rabelo Ferreira <arf92@live.com>
2019-03-24 12:41:40 -0300

    #2 Adicao de teste padrão de spock

--- certificate-validator/src/test/groovy/dev/gojava/certificatevalidator/ui/controller/HomeControllerTest.groovy
+++ certificate-validator/src/test/groovy/dev/gojava/certificatevalidator/ui/controller/HomeControllerTest.groovy
@@ -3,11 +3,12 @@ package dev.gojava.certificatevalidator.ui.controller
 import groovy.transform.CompileStatic
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
+import spock.lang.Specification
 
 @CompileStatic
 @EqualsAndHashCode(callSuper = true)
 @ToString(includeFields = true, includeSuper = true)
-class HomeControllerTest extends spock.lang.Specification {
+class HomeControllerTest extends Specification {
 
 	void setup() {
 	}
