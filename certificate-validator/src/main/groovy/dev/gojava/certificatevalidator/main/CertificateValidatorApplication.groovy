8e7dfd65bceb1da85a57a44575abe4d7028e7849
Alex Rabelo Ferreira <arf92@live.com>
2019-04-14 20:38:07 -0300

    #16 Add scan de pacote para encontrar components a ser injetado

--- certificate-validator/src/main/groovy/dev/gojava/certificatevalidator/main/CertificateValidatorApplication.groovy
+++ certificate-validator/src/main/groovy/dev/gojava/certificatevalidator/main/CertificateValidatorApplication.groovy
@@ -2,8 +2,10 @@ package dev.gojava.certificatevalidator.main
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan("dev.gojava.certificatevalidator")
 class CertificateValidatorApplication {
 
 	static void main(String[] args) {
