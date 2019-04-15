3f6dfa1970ac618e1f71853258725b16b87a4fbb
Alex Rabelo Ferreira <arf92@live.com>
2019-04-14 21:19:19 -0300

    #16 Uso de templates tymelife para views do controller

--- certificate-validator/src/main/groovy/dev/gojava/certificatevalidator/controler/ValidatorController.groovy
+++ certificate-validator/src/main/groovy/dev/gojava/certificatevalidator/controler/ValidatorController.groovy
@@ -6,23 +6,37 @@ import groovy.util.logging.Log4j2
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
+@RequestMapping("/validator")
 @CompileStatic
 @Log4j2
 class ValidatorController {
 
+	private static final String INVALID_TEMPLATE_PAGE = "page/validator/invalid"
+	private static final String VALID_TEMPLATE_PAGE = "page/validator/valid"
+
 	@Autowired
 	CertificateService certificateService
 
-	@GetMapping("/validateToken")
+	@GetMapping("/")
+	@ResponseBody
+	@SuppressWarnings("unused")
+	String getIndex() {
+		return "/page/validator"
+	}
+
+	@GetMapping(name = "/validate")
+	@SuppressWarnings("unused")
 	String validate(@RequestParam("token") String token) {
 		boolean tokenExist = certificateService.tokenExist(token)
 		if (!tokenExist) {
-			return "invalid"
+			return INVALID_TEMPLATE_PAGE
 		}
 
-		return "valid"
+		return VALID_TEMPLATE_PAGE
 	}
 }
