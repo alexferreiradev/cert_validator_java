543b344b87e1aab04dee19a83bcdac6f608e68f1
Alex Rabelo Ferreira <arf92@live.com>
2019-05-25 18:02:15 -0300

    #22 Adicao de controller para validator

--- certificate-validator/src/main/groovy/dev/gojava/certificatevalidator/controler/ValidatorController.groovy
+++ certificate-validator/src/main/groovy/dev/gojava/certificatevalidator/controler/ValidatorController.groovy
@@ -2,11 +2,15 @@ package dev.gojava.certificatevalidator.controler
 
 import dev.gojava.certificatevalidator.service.CertificateService
 import groovy.transform.CompileStatic
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.ModelAndView
 
 @Controller
 @RequestMapping("/validator")
@@ -18,23 +22,35 @@ class ValidatorController {
 	private static final String INVALID_TEMPLATE_PAGE = "/page/validator/invalid"
 	private static final String VALID_TEMPLATE_PAGE = "/page/validator/valid"
 
+	private static final Logger LOG = LoggerFactory.getLogger(ValidatorController)
+
 	@Autowired
 	CertificateService certificateService
 
 	@GetMapping("/")
 	@SuppressWarnings("unused")
-	String getIndex() {
-		return "/page/validator"
+	ModelAndView getIndex(ModelAndView modelAndView) {
+		modelAndView.viewName = "/page/validator/validator"
+		return modelAndView
 	}
 
-	@GetMapping("/validate")
+	@PostMapping("/validate")
 	@SuppressWarnings("unused")
-	String validate(@RequestParam("token") String token) {
-		boolean tokenExist = certificateService.tokenExist(token)
-		if (!tokenExist) {
-			return INVALID_TEMPLATE_PAGE
+	ModelAndView validate(@RequestParam("token") String token, ModelAndView modelAndView) {
+		boolean tokenExist = false
+		try {
+			tokenExist = certificateService.tokenExist(token)
+		} catch (Exception e) {
+			LOG.error("Erro ao tentar validar token: ", e)
+			modelAndView.viewName = "error"
 		}
 
-		return VALID_TEMPLATE_PAGE
+		if (!tokenExist) {
+			modelAndView.viewName = INVALID_TEMPLATE_PAGE
+		} else {
+			modelAndView.viewName = VALID_TEMPLATE_PAGE
+		}
+
+		return modelAndView
 	}
 }
